41d926bee5ed63c3ba5b0eca01fd10bcfac9550c
cm2unguenole-afk <cm2unguenole@gmail.com>
2026-07-06 16:19:23 +0200

    Update galerie-v22.js

--- galerie-v22.js
+++ galerie-v22.js
@@ -1,1 +1,21 @@
-galerie-v22.js
+document.addEventListener("DOMContentLoaded", function () {
+
+    const images = document.querySelectorAll(".post-body img");
+
+    console.log("Nombre d'images trouvées :", images.length);
+
+    images.forEach(function(img, index){
+
+        if(index < 4){
+
+            img.style.width = "100%";
+            img.style.height = "auto";
+            img.style.maxWidth = "100%";
+
+            img.src = img.src.replace("/s320/","/s1200/");
+
+        }
+
+    });
+
+});
